14a13f325e53f4969a67c4cd6a56835a9ce6dc3f
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2024-09-11 14:06:24 -0700

    all the scenarios added - too high, too low, correct answer

--- starter/script.js
+++ starter/script.js
@@ -10,5 +10,14 @@ document.querySelector('.check').addEventListener('click', function () {
   // if theres no input provided
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+    // if guess is the same as secret number
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+    // if guess is greater than secret number
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    // nif guess is lower than secret number
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
   }
 });
