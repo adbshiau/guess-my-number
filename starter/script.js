e2feef5f7200717342ca404e108e64e471a71c61
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2024-09-11 13:51:41 -0700

    first scenario implemented - check if theres no number provided

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // if theres no input provided
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
