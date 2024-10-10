13fbda561f94ea4fc73fd4843c9ba79312c3c9d3
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2024-10-09 20:10:37 -0700

    implemented high score functionality

--- starter/script.js
+++ starter/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
@@ -27,6 +28,12 @@ document.querySelector('.check').addEventListener('click', function () {
     // css manipulation
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
     // if guess is greater than secret number
   } else if (guess > secretNumber) {
     // run if score is more than 1
